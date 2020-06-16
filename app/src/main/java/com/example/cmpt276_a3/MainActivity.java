6c22b2510dbc7703b7ce580691bc5bc81f84ac88
reecem <reece_mcgowan@sfu.ca>
2020-06-15 17:40:33 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/cmpt276_a3/MainActivity.java
+++ app/src/main/java/com/example/cmpt276_a3/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Adding this line to test merge master -> 1-add-welcome-screen
     }
 }
