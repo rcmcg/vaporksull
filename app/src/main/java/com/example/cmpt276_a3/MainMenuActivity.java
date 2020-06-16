690d5b9b349af5966a1fe0ce05253850f0fc3edf
Reece McGowan <reecem@sfu.ca>
2020-06-16 08:44:36 -0700

    Wired buttons for Play, Options, and Help from Main Menu

--- app/src/main/java/com/example/cmpt276_a3/MainMenuActivity.java
+++ app/src/main/java/com/example/cmpt276_a3/MainMenuActivity.java
@@ -5,13 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    public static final int LAUNCH_GAME = 0;
+    public static final int LAUNCH_OPTIONS = 1;
+    public static final int LAUNCH_HELP = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setupPlayButton();
+        setupOptionsButton();
+        setupHelpButton();
+    }
+
+    private void setupPlayButton() {
+        Button btn = findViewById(R.id.btnPlay);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch game
+                // Get the Singleton with the GameData inside PlayGameActivity
+                Intent intent = PlayGameActivity.makeIntent(MainMenuActivity.this);
+                startActivityForResult(intent, LAUNCH_GAME);
+            }
+        });
+    }
+
+    private void setupOptionsButton() {
+        Button btn = findViewById(R.id.btnOptions);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch options
+                Intent intent = OptionsActivity.makeIntent(MainMenuActivity.this);
+                startActivityForResult(intent, LAUNCH_OPTIONS);
+            }
+        });
+    }
+
+    private void setupHelpButton() {
+        Button btn = findViewById(R.id.btnHelp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch options
+                Intent intent = HelpActivity.makeIntent(MainMenuActivity.this);
+                startActivityForResult(intent, LAUNCH_HELP);
+            }
+        });
     }
 
     public static Intent makeIntent(Context context) {
--- app/src/main/java/com/example/cmpt276_a3/OptionsActivity.java
+++ app/src/main/java/com/example/cmpt276_a3/OptionsActivity.java
@@ -2,6 +2,8 @@ package com.example.cmpt276_a3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class OptionsActivity extends AppCompatActivity {
@@ -11,4 +13,9 @@ public class OptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
     }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, OptionsActivity.class);
+    }
 }
+
