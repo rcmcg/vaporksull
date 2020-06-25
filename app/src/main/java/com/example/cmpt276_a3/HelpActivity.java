4cc3f20b43096fa04a362f14589a592a438c8944
Reece McGowan <reecem@sfu.ca>
2020-06-24 17:45:16 -0700

    Help page, added a scroll bar, hyperlinks, and made the font bigger

--- app/src/main/java/com/example/cmpt276_a3/HelpActivity.java
+++ app/src/main/java/com/example/cmpt276_a3/HelpActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -12,6 +15,10 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        // Set hyperlinks to be clickable
+        TextView links = findViewById(R.id.txtHelpCredits);
+        links.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public static Intent makeIntent(Context context) {
